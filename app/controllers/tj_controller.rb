2b5e4083cd7c7f5c8ca29437bd5270abda8bf8eb
ivanov e.v <nullsesion@yandex.ru>
2014-05-08 12:17:16 +0400

    базовый функционал готов

---
+++ app/controllers/tj_controller.rb
@@ -0,0 +1,13 @@
+class TjController < ApplicationController
+  unloadable
+
+
+  def index
+	if User.current.logged?
+		render text: Issue.where(assigned_to_id: User.current.id).where(status_id: 1).count.to_s
+	else
+		render text: "0"
+	end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+
+get 'tj', :to => 'tj#index'
