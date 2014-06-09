1b5635a2819ca3f7ad26cdd77e716bf120a2fc75
ivanov e.v <nullsesion@yandex.ru>
2014-06-09 14:43:12 +0400

    add setting page

--- init.rb
+++ init.rb
@@ -5,5 +5,6 @@ Redmine::Plugin.register :new_tasks do
   version '0.0.1'
   url ''
   author_url 'http://country-coder.blogspot.ru/'
+  settings :default => {'empty' => true}, :partial => 'settings/new_tasks'
 end
 require 'new_tasks/view_hooks'
